ce9a108283d8da37a99f4c204c2b6dd484c3d858
Debojyoti Mukherjee <59368291+debojyotimukh@users.noreply.github.com>
2020-10-01 13:37:13 +0530

    mood analyzer testingTC 1.1,1.2

--- src/test/java/MoodAnalyzerTest.java
+++ src/test/java/MoodAnalyzerTest.java
@@ -4,10 +4,17 @@ import org.junit.Test;
 
 public class MoodAnalyzerTest {
     @Test
-    public void testMoodAnalysis() throws Exception{
+    public void testMoodAnalysis1() throws Exception{
         MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
 
-        String mood=moodAnalyzer.analyseMood("This is a sad message");
+        String mood=moodAnalyzer.analyseMood("I am in sad mood");
         Assert.assertThat(mood, CoreMatchers.is("SAD"));
+    }
+
+    @Test
+    public void testMoodAnalysis2() throws Exception{
+        MoodAnalyzer moodAnalyzer=new MoodAnalyzer();
+        String mood=moodAnalyzer.analyseMood("I am in any mood");
+        Assert.assertThat(mood,CoreMatchers.is("HAPPY"));
     }
 }
